dd8aa678de09a5ba03b76b89eef143dc04e7d7e6
ellensur <essurovtseva@gmail.com>
2026-05-09 10:24:34 +0200

    new approach

--- scratch/test-contacts.js
+++
@@ -1,9 +0,0 @@
-import { handleQueryContacts } from '../src/actions/query/contacts.js';
-
-async function test() {
-  console.log('--- Testing Contacts Query (Blockscout Integration) ---');
-  const res = await handleQueryContacts();
-  console.log(res.text);
-}
-
-test();
